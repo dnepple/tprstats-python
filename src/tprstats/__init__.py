55ba0d4cc5d8eefc58caa45b16f96afba4328fcb
Stephen Epple <7917874+spacerobotsteve@users.noreply.github.com>
2024-11-17 01:18:41 -0500

    refactor: formatting

--- src/tprstats/__init__.py
+++ src/tprstats/__init__.py
@@ -1,12 +1,7 @@
-__all__ = [
-    "lm",
-    "summary"
-]
+__all__ = ["lm", "summary"]
 # read version from installed package
 from importlib.metadata import version
+
 __version__ = version("tprstats")
 
-from .regression import (
-    lm,
-    summary
-)
+from .regression import lm, summary
