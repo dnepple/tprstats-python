b7f8b8b42493ab79fd15bd0d7205c0e5cda4ab0f
Stephen Epple <7917874+spacerobotsteve@users.noreply.github.com>
2024-11-17 05:39:04 +0000

    refactor: import paths

--- src/tprstats/__init__.py
+++ src/tprstats/__init__.py
@@ -1,3 +1,12 @@
+__all__ = [
+    "lm",
+    "summary"
+]
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("tprstats")
+
+from .regression import (
+    lm,
+    summary
+)
