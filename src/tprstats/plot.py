b1a8097fa67b4e6915e5b937794fbae12d5f89a5
Stephen Epple <7917874+spacerobotsteve@users.noreply.github.com>
2024-11-21 05:05:32 +0000

    feat(control charts): cleanup

--- src/tprstats/plot.py
+++ src/tprstats/plot.py
@@ -1,32 +1,12 @@
-import numpy as np
-import random as rd
+from numpy import mean
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def tester(x):
-    rd.seed(32)
-    return [rd.randint(0, x) for i in range(0, 5)]
-
-
 def plot_objective_function(fn, start, stop):
     xvar = range(start, stop)
-    fn_x = list(map(fn, xvar))
-    yvar = list(map(np.mean, fn_x))
+    yvar = [mean(fn(x)) for x in xvar]
     data = pd.DataFrame({"xvar": xvar, "yvar": yvar})
     plt.plot("yvar", "xvar", data=data)
-
-
-plot_objective_function(tester, 1, 20)
-
-# plotObjectiveFunction <- function(fn, from, to) {
-#   results <- data.frame("xvar" = from:to)
-#   # fn is assumed to return a vector and the mean is taken
-#   results$yvar <- sapply(X = results$xvar, FUN = function(x) {
-#     mean(fn(x))
-#   })
-
-#   graphics::plot(yvar~xvar, data = results, xlab = "x", ylab = "y", pch = 16)
-
-#   return(results)
-# }
+    plt.xlabel("x")
+    plt.ylabel("y")
