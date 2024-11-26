f0036fb5a3f819c7fd916d00d524c86d5b3c9a3d
Stephen Epple <7917874+spacerobotsteve@users.noreply.github.com>
2024-11-26 21:20:07 +0000

    feat(probit): Add probit model

--- src/tprstats/model_wrapper.py
+++ src/tprstats/model_wrapper.py
@@ -127,9 +127,9 @@ class LogitModel(StatsmodelsModelWrapper, BinaryChoiceMixin):
 class ProbitModel(StatsmodelsModelWrapper, BinaryChoiceMixin):
     def __init__(self, formula, data):
         super().__init__()
-        self._model = smf.logit(formula, data)
+        self._model = smf.probit(formula, data)
         self._result = self._model.fit()
-        self._summary = self._result.summary(slim=True)
+        self._summary = self._result.summary()
         self._formula = formula
         self._data = data
 
