e06d51e5cd03e021d2508f2add120c71d88c508c
Stephen Epple <7917874+spacerobotsteve@users.noreply.github.com>
2024-11-27 19:07:57 +0000

    remove r style api

--- src/tprstats/__init__.py
+++ src/tprstats/__init__.py
@@ -4,6 +4,5 @@ from importlib.metadata import version
 
 __version__ = version("tprstats")
 
-from .regression import lm, summary
 from .model_wrapper import model
 from .control_charts import control_chart, control_chart_binary
