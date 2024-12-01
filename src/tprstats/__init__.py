c906cc5248666defb05ab40faafe2744bfaec54a
Stephen Epple <7917874+spacerobotsteve@users.noreply.github.com>
2024-11-30 19:15:38 -0500

    Add docstrings and rename submodules (#10)
    * refactor: rename submodule
    
    * docs: add docstrings for plots submodule 
    
    * docs: add docstrings for models submodule
    
    * refactor: remove unused exports (lm and summary no longer exist)

--- src/tprstats/__init__.py
+++ src/tprstats/__init__.py
@@ -1,8 +1,8 @@
-__all__ = ["lm", "summary", "model", "control_chart", "control_chart_binary"]
+__all__ = ["model", "control_chart", "control_chart_binary"]
 # read version from installed package
 from importlib.metadata import version
 
 __version__ = version("tprstats")
 
-from .model_wrapper import model
-from .control_charts import control_chart, control_chart_binary
+from .models import model
+from .plots import control_chart, control_chart_binary
