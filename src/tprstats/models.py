91af82bc7f7c6a213ba58b026bdac5f7908ddad0
Stephen Epple <7917874+spacerobotsteve@users.noreply.github.com>
2024-12-17 17:49:12 -0500

    Refactor models (#20)
    * refactor: cross-section and time series linear models
    
    * refactor: remove unused base classes
    
    * refactor: binary choice models
    
    * refactor: time series and cross section models
    
    * feat(time_series): handle std err corrections
    
    * feat(wald_test): output

--- src/tprstats/models.py
+++ src/tprstats/models.py
@@ -1,4 +1,3 @@
-from abc import ABC, abstractmethod
 import statsmodels.formula.api as smf
 from numpy import mean as numpy_mean
 from numpy import number as numpy_number
@@ -8,60 +7,21 @@ import pandas as pd
 from scipy import stats as scipy_stats
 from .plots import _plot_actual_fitted
 from statsmodels.stats.diagnostic import linear_reset
+from statsmodels.formula.api import ols
 
 # numpy required for use in patsy formulae
 from numpy import log, exp, floor, ceil, trunc, absolute  # noqa: F401
 
 
-class _ModelWrapper(ABC):
-    """Defines a uniform interface for interacting with model objects. All model objects should, at a minimum, implement this interface."""
+class LinearModels:
+    """Wrapper class for statsmodels RegressionResults."""
 
-    @abstractmethod
-    def model(self):
-        """Returns a model."""
-        pass
-
-    @abstractmethod
-    def result(self):
-        """Returns the result of fitting the model."""
-        pass
-
-    @abstractmethod
-    def summary(self):
-        """Return summary statistics for the given model."""
-        pass
-
-    @abstractmethod
-    def formula(self):
-        """Returns the model's formula."""
-        pass
-
-    @abstractmethod
-    def data(self):
-        """Returns the dataframe used to fit the model."""
-        pass
-
-
-class _StatsmodelsModelWrapper(_ModelWrapper):
-    """Wraps models from the statsmodels package."""
-
-    def model(self):
-        return self._model
-
-    def result(self):
-        return self._result
+    def __init__(self, formula, data, **kwargs):
+        self.model = ols(formula, data)
+        self.data = data
 
     def summary(self):
-        return self._summary
-
-    def formula(self):
-        return self._formula
-
-    def predict(self, exog=None):
-        return self._result.predict(exog)
-
-    def data(self):
-        return self._data
+        return self.result.summary(slim=True)
 
     def cite(self):
         """Returns citations for the source of the model."""
@@ -81,13 +41,9 @@ class _StatsmodelsModelWrapper(_ModelWrapper):
         print(citation_bibtex_entry)
         return
 
-
-class _LinearModels(_StatsmodelsModelWrapper):
-    """An abstract class defining general methods for linear models."""
-
     def prediction_intervals(self, exog=None, alpha=0.05):
         """Returns a table of prediction intervals."""
-        predictions = self._result.get_prediction(exog)
+        predictions = self.result.get_prediction(exog)
         prediction_table = predictions.summary_frame(alpha=alpha)
         prediction_table = prediction_table[["mean", "obs_ci_lower", "obs_ci_upper"]]
         prediction_table = prediction_table.rename(
@@ -103,24 +59,24 @@ class _LinearModels(_StatsmodelsModelWrapper):
         """Returns a table of the standardized coefficients."""
         # standardize data
         df_z = (
-            self._data.select_dtypes(include=[numpy_number])
+            self.data.select_dtypes(include=[numpy_number])
             .dropna()
             .apply(scipy_stats.zscore)
         )
-        result = smf.ols(self._formula, data=df_z).fit()
+        result = smf.ols(self.model.formula, data=df_z).fit()
         # drop 'Intercept
         return result.params[1:]
 
     def elasticities(self):
         """Returns a table of the elasticities."""
         # drop 'Intercept' from rhs
-        rhs = self._model.exog_names[1:]
-        lhs = self._model.endog_names
+        rhs = self.model.exog_names[1:]
+        lhs = self.model.endog_names
 
-        means = self._data[rhs].mean()
-        y_mean = self._data[lhs].mean()
+        means = self.data[rhs].mean()
+        y_mean = self.data[lhs].mean()
         # drop 'Intercept'
-        coefs = self._result.params[1:]
+        coefs = self.result.params[1:]
 
         elasticities = coefs * (means / y_mean)
 
@@ -131,7 +87,7 @@ class _LinearModels(_StatsmodelsModelWrapper):
         std_coefs = self.standardized_coefficients()
         elasticities = self.elasticities()
         # drop 'Intercept'
-        coefs = self._result.params[1:]
+        coefs = self.result.params[1:]
         table = pd.DataFrame(
             dict(coefs=coefs, std_coefs=std_coefs, elasticities=elasticities)
         )
@@ -139,10 +95,10 @@ class _LinearModels(_StatsmodelsModelWrapper):
 
     def plot_actual_fitted(self):
         """Plots actual values and predicted values with upper and lower prediction intervals for associated linear model."""
-        y_id = self._model.endog_names
-        y = self._data[y_id]
-        X = self._data[self._model.exog_names[1:]]
-        Pred_and_PI = self._result.get_prediction(X).summary_frame(alpha=0.05)
+        y_id = self.model.endog_names
+        y = self.data[y_id]
+        X = self.data[self.model.exog_names[1:]]
+        Pred_and_PI = self.result.get_prediction(X).summary_frame(alpha=0.05)
         predicted = Pred_and_PI["mean"]
         lower = Pred_and_PI["obs_ci_lower"]
         upper = Pred_and_PI["obs_ci_upper"]
@@ -159,9 +115,8 @@ class _LinearModels(_StatsmodelsModelWrapper):
         """
         # Statsmodels FutureWarning: The behavior of wald_test will change after 0.14 to returning scalar test statistic values.
         # To get the future behavior now, set scalar to True.
-        wald_test = self._result.wald_test(r_matrix=hypothesis, use_f=True, scalar=True)
-        print("Wald Test Statistic: ", wald_test.statistic)
-        print("p-value: ", wald_test.pvalue)
+        wald_test = self.result.wald_test(r_matrix=hypothesis, use_f=True, scalar=True)
+        print("p-value: ", round(wald_test.pvalue, 4))
         return
 
     def ramsey_test(self):
@@ -172,7 +127,7 @@ class _LinearModels(_StatsmodelsModelWrapper):
         Returns:
             : Frame with columns [power, pvalue]
         """
-        fit = self._model.fit()
+        fit = self.model.fit()
         pvalues = [
             {
                 "power": 2,
@@ -186,12 +141,12 @@ class _LinearModels(_StatsmodelsModelWrapper):
         return pd.DataFrame(pvalues)
 
     def coefficients_and_covariance(self):
-        return (self._result.params, self._result.cov_params())
+        return (self.result.params, self.result.cov_params())
 
     def coefficients_and_covariance_table(self):
         coefs, cov_matrix = self.coefficients_and_covariance()
         combined = column_stack((coefs, cov_matrix))
-        rhs = self._model.exog_names  # keep Intercept
+        rhs = self.model.exog_names  # keep Intercept
         table = pd.DataFrame(combined, columns=["coefs", *rhs])
         table.insert(0, "", rhs)
         return table
@@ -200,38 +155,33 @@ class _LinearModels(_StatsmodelsModelWrapper):
         coefs, cov_matrix = self.coefficients_and_covariance()
         rng = np_random.default_rng()
         coef_draws = rng.multivariate_normal(coefs, cov_matrix, size)
-        rhs = self._model.exog_names  # keep Intercept
+        rhs = self.model.exog_names  # keep Intercept
         return pd.DataFrame(coef_draws, columns=rhs)
 
+    def __getattr__(self, name):
+        # Delegates any method calls not explicitly defined here to the wrapped object
+        return getattr(self.result, name)
 
-class CrossSectionalLinearModel(_LinearModels):
-    """A concrete class for cross-sectional linear models."""
+
+class TimeSeriesLinearModel(LinearModels):
+    def __init__(self, formula, data, maxlags=2, use_correction=True, **kwargs):
+        super().__init__(formula, data, **kwargs)
+        self.result = self.model.fit().get_robustcov_results(
+            cov_type="HAC", maxlags=maxlags, use_correction=use_correction
+        )
+
+
+class CrossSectionLinearModel(LinearModels):
+    def __init__(self, formula, data, **kwargs):
+        super().__init__(formula, data, **kwargs)
+        self.result = self.model.fit(cov_type="HC1")
+
+
+class BinaryChoiceModels:
+    """Binary Choice Models."""
 
     def __init__(self, formula, data):
-        super().__init__()
-        self._model = smf.ols(formula, data)
-        self._result = self._model.fit().get_robustcov_results(cov_type="HC1")
-        self._summary = self._result.summary(slim=True)
-        self._formula = formula
-        self._data = data
-
-
-class TimeSeriesLinearModel(_LinearModels):
-    """A concrete class for time series linear models."""
-
-    def __init__(self, formula, data, maxlags=1):
-        super().__init__()
-        self._model = smf.ols(formula, data)
-        self._result = self._model.fit().get_robustcov_results(
-            cov_type="HAC", maxlags=maxlags
-        )
-        self._summary = self._result.summary(slim=True)
-        self._formula = formula
-        self._data = data
-
-
-class _BinaryChoiceModels(_StatsmodelsModelWrapper):
-    """An abstract class defining general methods for binary choice models."""
+        raise NotImplementedError
 
     def predict_and_rank(self, exog):
         prospects = exog
@@ -245,7 +195,7 @@ class _BinaryChoiceModels(_StatsmodelsModelWrapper):
         else:
             threshold = numpy_mean(self.predict())
 
-        frequency = self._result.pred_table(threshold).flatten().tolist()
+        frequency = self.result.pred_table(threshold).flatten().tolist()
         print(frequency)
         table = pd.DataFrame(
             {
@@ -258,38 +208,36 @@ class _BinaryChoiceModels(_StatsmodelsModelWrapper):
         return table
 
     def marginal_effects(self):
-        marginal_effects_at_the_mean = self._result.get_margeff(at="overall")
+        marginal_effects_at_the_mean = self.result.get_margeff(at="overall")
         return marginal_effects_at_the_mean.summary()
 
-    def wald_test(self, hypothesis):
-        wald_test = self._result.wald_test(r_matrix=hypothesis, scalar=True)
+    def wald_testing(self, hypothesis):
+        wald_test = self.result.wald_test(r_matrix=hypothesis, scalar=True)
         print("Wald Test Statistic: ", wald_test.statistic)
         print("p-value: ", wald_test.pvalue)
         return
 
-
-class LogitModel(_BinaryChoiceModels):
-    """A concrete class for Logit Models."""
-
-    def __init__(self, formula, data):
-        super().__init__()
-        self._model = smf.logit(formula, data)
-        self._result = self._model.fit()
-        self._summary = self._result.summary()
-        self._formula = formula
-        self._data = data
+    def __getattr__(self, name):
+        # Delegates any method calls not explicitly defined here to the wrapped object
+        return getattr(self.result, name)
 
 
-class ProbitModel(_BinaryChoiceModels):
-    """A concrete class for Probit Models."""
+class LogitModel(BinaryChoiceModels):
+    """Logit Model."""
 
     def __init__(self, formula, data):
-        super().__init__()
-        self._model = smf.probit(formula, data)
-        self._result = self._model.fit()
-        self._summary = self._result.summary()
-        self._formula = formula
-        self._data = data
+        self.model = smf.logit(formula, data)
+        self.result = self.model.fit()
+        self.data = data
+
+
+class ProbitModel(BinaryChoiceModels):
+    """Probit Models."""
+
+    def __init__(self, formula, data):
+        self.model = smf.probit(formula, data)
+        self.result = self.model.fit()
+        self.data = data
 
 
 def model(name, formula, data, **kwargs):
@@ -308,12 +256,9 @@ def model(name, formula, data, **kwargs):
     """
     match name:
         case "cs":
-            return CrossSectionalLinearModel(formula, data)
+            return CrossSectionLinearModel(formula, data)
         case "ts":
-            if "maxlags" in kwargs:
-                return TimeSeriesLinearModel(formula, data, maxlags=kwargs["maxlags"])
-            else:
-                return TimeSeriesLinearModel(formula, data)
+            return TimeSeriesLinearModel(formula, data)
         case "logit":
             return LogitModel(formula, data)
         case "probit":
